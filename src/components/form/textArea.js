b615d963e0a704d0d703e71310511d3ad4e2935b
Eugene <softwarearchitect19@gmail.com>
2024-06-27 20:47:39 +0300

    Update edit form.

---
+++ src/components/form/textArea.js
@@ -0,0 +1,23 @@
+import { forwardRef } from "react"
+
+const TextArea = forwardRef((props, ref) => {
+    return (
+        <div className="mb-3">
+            <label htmlFor={props.name} className="form-label">
+                {props.title}
+            </label>
+            <textarea
+                className={props.className}
+                id={props.name}
+                ref={ref}
+                name={props.name}
+                placeholder={props.placeholder}
+                onChange={props.onChange}
+                value={props.value}
+            />
+            <div className={props.errorDiv}>{props.errorMsg}</div>
+        </div>
+    )
+})
+
+export default TextArea;
